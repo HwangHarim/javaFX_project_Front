77770602e951ebdd2c6c5efc251ab57b589873b3
HwangHarim <scopan01@naver.com>
2024-02-20 23:09:32 +0900

    fix : main 화면 버튼 페이지 이동 연결

--- src/main/java/com/service/jw_project_front/JwFrontApplication.java
+++ src/main/java/com/service/jw_project_front/JwFrontApplication.java
@@ -1,11 +1,10 @@
 package com.service.jw_project_front;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class JwFrontApplication extends Application {
 
--- src/main/java/com/service/jw_project_front/controller/MainController.java
+++ src/main/java/com/service/jw_project_front/controller/MainController.java
@@ -1,48 +1,71 @@
 package com.service.jw_project_front.controller;
 
 import com.service.jw_project_front.service.TimeService;
+import java.io.IOException;
 import java.util.Objects;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
 public class MainController {
 
     @FXML
-    private BorderPane home;
+    private BorderPane main;
     @FXML
     private Label timeLabel;
+    @FXML
+    private StackPane centerPane;
 
-    public void initialize() {
+
+    private StackPane mainStack;
+
+    public void initialize() throws IOException {
         TimeService.viewTime(timeLabel);
+        mainStack = getMainStack();
+        centerPane.getChildren().add(mainStack);
+
+    }
+
+    public StackPane getMainStack() throws IOException {
+        StackPane stack = null;
+        stack = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader()
+            .getResource("com/service/jw_project_front/main_stack_page.fxml")));
+        return stack;
     }
 
 
+    /**
+     *
+     */
     @FXML
-    public void moveAutoCookPage(){
+    public void moveAutoCookPage() {
         movePage("auto-cook-view");
     }
+
     @FXML
-    public void moveSelfCookPage(){
+    public void moveSelfCookPage() {
         movePage("self-cook-view");
     }
+
     @FXML
-    public void moveCleanOvenPage(){
+    public void moveCleanOvenPage() {
         movePage("clean-oven-view");
     }
+
     @FXML
-    public void moveLogCookPage(){
+    public void moveLogCookPage() {
         movePage("log-cook-view");
     }
 
     public void movePage(String page) {
         try {
-            Parent sub = FXMLLoader.load(Objects.requireNonNull(
+            Pane sub = FXMLLoader.load(Objects.requireNonNull(
                 getClass().getClassLoader()
                     .getResource("com/service/jw_project_front/" + page + ".fxml")));
-            home.setCenter(sub);
+            main.getChildren().add(sub);
 
         } catch (Exception e) {
             e.printStackTrace();
